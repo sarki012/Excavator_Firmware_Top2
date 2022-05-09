469de3bc3b5b90e80e519aa24fd83ae69cce267e
sarki012 <60797267+sarki012@users.noreply.github.com>
2022-05-09 17:25:28 -0500

    Buttery Smooth Motor Control

---
+++ ExcavatorHeaders/main.h
@@ -0,0 +1,21 @@
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef MAIN_H
+#define	MAIN_H
+
+#include <xc.h> // include processor files - each processor file is guarded. 
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "croutine.h"
+#include "FreeRTOSConfig.h"
+
+#define NUM_AVG 100
+void init(void);
+void curlTask(void *pvParameters);
+void boomTask(void *pvParameters);
+int charToInt(char digit2, char digit1, char digit0);
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
