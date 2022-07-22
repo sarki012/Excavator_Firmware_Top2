dc3088bee65c0427679c8ccd8f203e0db5c1dd50
sarki012 <60797267+sarki012@users.noreply.github.com>
2022-07-22 12:46:08 -0500

    SAMPLE_RATE = 2

--- ExcavatorHeaders/main.h
+++ ExcavatorHeaders/main.h
@@ -11,13 +11,15 @@
 #include "croutine.h"
 #include "FreeRTOSConfig.h"
 
-#define SAMPLE_RATE 20
-#define LOOPS 2500
+#define SAMPLE_RATE 2      //Was 1000 Was 20
+#define LOOPS 2000     //2000 is good. Was 500. Was 2500. Tried 2000. Too fast. Last try: 2500. Ok but still too fast. Last try 3000
+#define LOOPS_CURL 200     //Want the curl to be faster
 void init(void);
-void curlTask(void *pvParameters);
-void boomTask(void *pvParameters);
-void stickTask(void *pvParameters);
-int charToInt(char digit2, char digit1, char digit0);
-
+void curlThread(void *pvParameters);
+void boomThread(void *pvParameters);
+void stickThread(void *pvParameters);
+void radioThread(void *pvParameters);
+int charToInt(char digit3, char digit2, char digit1, char digit0);
+void delay(int numLoops);
 #endif	/* XC_HEADER_TEMPLATE_H */
 
